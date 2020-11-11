6b0d70a9339f1aa1fa445895f56dac669f12cc0b
Saenja <57174531+Saenja@users.noreply.github.com>
2020-11-11 13:38:53 +0100

    Update aufgabe_2.py

--- aufgabe_2.py
+++ aufgabe_2.py
@@ -6,15 +6,15 @@ print(l ,t)
 
 if l>=10:
 	if l<100:
-		A= l/10
+		A= l//10
 		B= l%10
 
 		BA= B*10+A
 		print(int(BA))
 
-		Summe= print('A + B =',int(A+B))
-		Subtraktion = print('A - B =',int(A-B))
-		Produkt= print('A * B =',int(A * B))
+		Summe= print('A + B =',A+B)
+		Subtraktion = print('A - B =',A-B)
+		Produkt= print('A * B =',A * B)
 		Division = print('B / A =',B/A)
 	else:
 		print('Zeichenkette zu lang')
